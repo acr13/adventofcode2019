003b9af4881c98f976ca461775c514747a68d814
Alex Richardson <alex@alexcr.com>
2019-12-18 08:12:55 -0500

    Day 10 p2

--- 10/index.js
+++ 10/index.js
@@ -6,9 +6,9 @@ const lineReader = require('readline').createInterface({
 });
 const parseLine = line => map.push(line.split(''));
 lineReader.on('line', line => parseLine(line));
-lineReader.on('close', () => partOne(map));
+lineReader.on('close', () => run(map));
 
-const partOne = (map) => {
+const run = (map) => {
   const ROWS = map.length;
   const COLS = map[0].length;
   const asteroids = [];
@@ -22,6 +22,7 @@ const partOne = (map) => {
   }
 
   let max = -1;
+  let bestLocationIndex = -1;
   for (let i = 0; i < asteroids.length; i++) {
     const asteroid = asteroids[i];
     const angles = new Set();
@@ -37,10 +38,58 @@ const partOne = (map) => {
     visibleAsteroids = angles.size;
     if (visibleAsteroids > max) {
       max = visibleAsteroids;
+      bestLocationIndex = i;
     }
   }
 
   console.log('Part one:', max);
-  return max;
-};
+  
+  // from our start position
+  const [row, col] = asteroids[bestLocationIndex];
+  const degreeSet = new Set();
 
+  // create a hash of
+  // { [angle]: [asteroids at this angle...] }
+  const asteroidsAroundMe = {};
+  for (let i = 0; i < asteroids.length; i++) {
+    if (i !== bestLocationIndex) {
+      const [row2, col2] = asteroids[i];
+      const angle = Math.atan2(row2 - row, col2 - col);
+      const degrees = angle * (180 / Math.PI);
+
+      if (!asteroidsAroundMe[degrees]) {
+        asteroidsAroundMe[degrees] = [];
+      }
+      asteroidsAroundMe[degrees].push({
+        row: row2,
+        col: col2,
+        degrees,
+        distance: Math.hypot(row - row2, col - col2),
+      });
+
+      degreeSet.add(degrees);
+
+      if (asteroidsAroundMe[degrees].length > 1) {
+        asteroidsAroundMe[degrees].sort((a, b) => b.distance - a.distance);
+      }
+    }
+  }
+
+  const sortedDegrees = [...degreeSet].sort((a, b) => a - b);
+  let currentIndex = sortedDegrees.findIndex(degree => degree === -90);
+  let counter = 0;
+  let target = null;
+
+  while (counter < 200) {
+    let currentDegree = sortedDegrees[currentIndex];
+
+    if (asteroidsAroundMe[currentDegree].length) {
+      target = asteroidsAroundMe[currentDegree].pop();
+      counter++;
+    }
+
+    currentIndex = currentIndex < (sortedDegrees.length - 1) ? currentIndex + 1 : 0;
+  }
+
+  console.log('Part two', ((target.col * 100) + target.row));
+};
