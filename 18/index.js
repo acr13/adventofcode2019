84b245d8a778dfe32eb1a941d52e3fc5d31ee5f4
Alex Richardson <alex@alexcr.com>
2019-12-25 08:39:21 -0500

    Day 18 p2 and 25

--- 18/index.js
+++ 18/index.js
@@ -3,12 +3,15 @@ const DC = [0, 1, 0, -1];
 const GRID = [];
 
 const lineReader = require('readline').createInterface({
-  input: require('fs').createReadStream('18/input.txt')
+  input: require('fs').createReadStream('18/test6.txt')
 });
 const parseLine = line => GRID.push(line.split(''));
 lineReader.on('line', line => parseLine(line));
 lineReader.on('close', () => {
-  console.log('Part one:', partOne(GRID));
+  // 6162
+  // console.log('Part one:', partOne(GRID));
+  const distances = calculateDistances(GRID);
+  // console.log('Part two:', partTwo(GRID));
 });
 
 const getKey = (row, col, keys) => {
@@ -18,8 +21,10 @@ const getKey = (row, col, keys) => {
 
 const partOne = (GRID) => {
   const allKeys = new Set();
-  let queue = [];
+  const queue = [];
 
+  // walk the grid, find our starting point
+  // also make a set of all of the keys
   for (let r = 0; r < GRID.length; r++) {
     for (let c = 0; c < GRID[0].length; c++) {
       if (GRID[r][c] === '@') {
@@ -54,7 +59,7 @@ const partOne = (GRID) => {
 
     const newKeys = new Set(keys);
 
-    // key
+    // key - pick it up and sort our list
     if ('a' <= GRID[r][c] && GRID[r][c] <= 'z') {
       newKeys.add(GRID[r][c]);
 
@@ -69,3 +74,41 @@ const partOne = (GRID) => {
   }
 };
 
+const partTwo = (GRID) => {
+  const allKeys = new Set();
+  const queue = [];
+  const starts = [];
+
+  // walk the grid, find our starting point
+  // also make a set of all of the keys
+  for (let r = 0; r < GRID.length; r++) {
+    for (let c = 0; c < GRID[0].length; c++) {
+      if (GRID[r][c] === '@') {
+        starts.push([r, c]);
+        queue.push([r, c, new Set(), 0]);
+      } else if ('a' <= GRID[r][c] && GRID[r][c] <= 'z') {
+        allKeys.add(GRID[r][c]);
+      }
+    }
+  }
+
+  const NUM_ALL_KEYS = allKeys.size;
+  const SEEN = new Set();
+
+  while (queue.length) {
+    const [r, c, keys, d] = queue.shift();
+    const key = getKey(r, c, keys);
+
+    for (let i = 0; i < 4; i++) {
+      queue.push([r + DR[i], c + DC[i], newKeys, d + 1]);
+    }
+  }
+
+  console.log(starts);
+};
+
+const calculateDistances = (GRID) => {
+  const distances = {};
+
+  return distances;
+};
